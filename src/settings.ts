59361b761e547fd4eff91be77c805c05bfceeadd
aviral-batra <68279653+aviral-batra@users.noreply.github.com>
2025-01-28 10:32:06 +0000

    Remove potentially unecessary if statement during fields dict regeneration. This was preventing correct regeneration of the fields dictionary.
    (if length of fields dictionary not equals note type length)

--- src/settings.ts
+++ src/settings.ts
@@ -350,16 +350,14 @@ export class SettingsTab extends PluginSettingTab {
 							plugin.note_types = await AnkiConnect.invoke('modelNames')
 							plugin.regenerateSettingsRegexps()
 							plugin.fields_dict = await plugin.loadFieldsDict()
-							if (Object.keys(plugin.fields_dict).length != plugin.note_types.length) {
-								new Notice('Need to connect to Anki to generate fields dictionary...')
-								try {
-									plugin.fields_dict = await plugin.generateFieldsDict()
-									new Notice("Fields dictionary successfully generated!")
-								}
-								catch(e) {
-									new Notice("Couldn't connect to Anki! Check console for error message.")
-									return
-								}
+							new Notice('Need to connect to Anki to generate fields dictionary...')
+							try {
+								plugin.fields_dict = await plugin.generateFieldsDict()
+								new Notice("Fields dictionary successfully generated!")
+							}
+							catch(e) {
+								new Notice("Couldn't connect to Anki! Check console for error message.")
+								return
 							}
 							await plugin.saveAllData()
 							this.setup_display()
